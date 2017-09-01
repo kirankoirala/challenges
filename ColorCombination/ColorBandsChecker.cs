8c237116ad770f2dd003abb1a259424f88c4e979
Kiran Koirala <kinbech@gmail.com>
2017-09-01 06:26:19 -0400

    * ColorBandsChecker.cs: Added ColorBandChecker to start actually   check if the combination is possible. Introduced NSubstitution as   well to mock the validator for NUnit test.
    * packages.config:
    * ColorCombination.csproj:
    * ColorBandsCheckerTests.cs:
    * ColorCombinationTests.csproj:

---
+++ ColorCombination/ColorBandsChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ColorCombination
+{
+    public class ColorBandsChecker
+    {
+        private IInputValidator _validator;
+        public ColorBandsChecker(IInputValidator validator)
+        {
+            _validator = validator;
+        }
+
+        public string CheckBands(UserInputColorSet colorSets){
+           
+            _validator.isValid(colorSets);
+            return null;//just to compile successfully for now
+        }
+    }
+}
---
+++ ColorCombinationTests/ColorBandsCheckerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using ColorCombination;
+using NSubstitute;
+
+namespace ColorCombinationTests
+{
+    [TestFixture()]
+    public class ColorBandsCheckerTests
+    {
+		private IInputValidator _validator;
+        private ColorBandsChecker _checker;
+
+		[SetUp]
+        public void SetUp(){
+            _validator = Substitute.For<IInputValidator>();
+            _checker = new ColorBandsChecker(_validator);
+        }
+
+        [Test()]
+        public void CheckBands_See_If_UserInputsAreValid_Valid()
+        {
+            var colorSetFromUser = new UserInputColorSet();
+            _checker.CheckBands(colorSetFromUser);
+
+            _validator.Received().isValid(colorSetFromUser);
+        }
+    }
+}
