cbe87cef72615fb9bf20ccb60cdb7b11b836f7a1
Kiran Koirala <kinbech@gmail.com>
2017-08-31 22:07:25 -0400

    Extracted validation to a private method

--- ColorCombination/InputValidator.cs
+++ ColorCombination/InputValidator.cs
@@ -7,22 +7,26 @@ namespace ColorCombination
 
         public bool isValid(UserInputColorSet userInput)
         {
-            
-            if (userInput == null){
+            ValidateInput(userInput);
+            return false;
+        }
+
+        private static void ValidateInput(UserInputColorSet userInput)
+        {
+            if (userInput == null)
+            {
                 throw new Exception("Invalid - Null input");
             }
 
             if (userInput.ColorSets == null)
-			{
-				throw new Exception("Invalid - Uninitialized Color Set");
-			}
+            {
+                throw new Exception("Invalid - Uninitialized Color Set");
+            }
 
             if (userInput.ColorSets.Length == 0)
-			{
-				throw new Exception("Invalid - Empty Color Set");
-			}
-          return false;
+            {
+                throw new Exception("Invalid - Empty Color Set");
+            }
         }
-
     }
 }
