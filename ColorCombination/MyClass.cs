7dbeb6d990ab8a8bc5afb169a39c591b4c56b9c6
Kiran Koirala <Kiran@Kirans-MacBook-Pro.local>
2017-08-31 21:00:03 -0400

    added user input class and started validator calss

---
+++ ColorCombination/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ColorCombination
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ ColorCombination/UserInputColorSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ColorCombination
+{
+    public class UserInputColorSet
+    {
+        public string[] ColorSets{get;set;}
+    }
+
+    public class InputValidator{
+
+        public bool inputIsValid(UserInputColorSet userInput){
+            return false;
+        }
+
+    }
+}
